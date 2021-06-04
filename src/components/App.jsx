180a07205bfd0b996cd14cd3837c38c368527789
SachinT-K <75876472+SachinT-K@users.noreply.github.com>
2021-06-04 12:53:44 +0530

    Ternary operator which contains expressions.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,14 @@
 import React from "react";
-import Login from "./Login"
+import Login from "./Login";
 
 var isLoggedIn = false;
 
-function renderConditionally(){
-  if (isLoggedIn === true){
-    return <h1>Hello</h1>
-  }else{
-    return <Login />
-  }
+function renderConditionally() {
+  return isLoggedIn === true ? <h1>Hello</h1> : <Login />;
 }
 
 function App() {
-  return (
-    <div className="container">
-      {renderConditionally()}
-    </div>
-  );
+  return <div className="container">{renderConditionally()}</div>;
 }
 
 export default App;
